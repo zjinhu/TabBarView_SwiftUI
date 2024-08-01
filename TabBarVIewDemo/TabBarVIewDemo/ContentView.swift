47b6e37c6b063d03230910e249f8d292cdcfd9ef
虎 <814030966@qq.com>
2024-08-01 11:15:35 +0800

    create

---
+++ TabBarVIewDemo/TabBarVIewDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TabBarVIewDemo
+//
+//  Created by 狄烨 on 2024/7/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TabBarVIewDemo/TabBarVIewDemo/TabBarVIewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarVIewDemoApp.swift
+//  TabBarVIewDemo
+//
+//  Created by 狄烨 on 2024/7/30.
+//
+
+import SwiftUI
+
+@main
+struct TabBarVIewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
