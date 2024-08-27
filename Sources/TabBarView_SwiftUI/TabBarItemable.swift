59d3bd4a781c829efb3014ec556acb8e9881739c
jinhu <814030966@qq.com>
2024-08-27 14:29:15 +0800

    更新

--- Sources/TabBarView_SwiftUI/TabBarItemable.swift
+++ Sources/TabBarView_SwiftUI/TabBarItemable.swift
@@ -12,6 +12,14 @@ public extension TabBarItemable {
     var selectedIcon: Image {
         return self.icon
     }
+    
+    var title: String {
+        return ""
+    }
+    
+    var color: Color {
+        return .clear
+    }
 
     static func isItemSelected(selector: Self, icon: Image, title: String, color: Color) -> Bool {
         return selector.icon == icon && selector.title == title && selector.color == color
